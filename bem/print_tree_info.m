3a4c32e94223ccb7f81b4981e0141eef3da84373
fiala <fiala@hit.bme.hu>
2012-06-06 21:03:06 +0200

    mivans

--- bem/print_tree_info.m
+++ bem/print_tree_info.m
@@ -1,20 +1,20 @@
 function print_tree_info(tree)
 
-disp(sprintf('Tree consists of %d levels (level 0 to %d)' , length(tree), length(tree)-1));
+fprintf(1, 'Tree consists of %d levels (level 0 to %d)\n' , length(tree), length(tree)-1);
 for l = 0 : length(tree)-1
     T = tree(l+1);
-    disp(sprintf('Level %d:', l));
-    disp(sprintf('\tCluster diameter: %f', T.diameter));
-    disp(sprintf('\tNumber of Clusters: %d', size(T.coord,1)));
+    fprintf(1, 'Level %d:\n', l);
+    fprintf(1, '\tCluster diameter: %f\n', T.diameter);
+    fprintf(1, '\tNumber of Clusters: %d\n', size(T.coord,1));
     if l > 0
-        disp(sprintf('\tMax. length of nearfield list: %d', size(T.nearfield,2)));
+        fprintf(1, '\tMax. length of nearfield list: %d\n', size(T.nearfield,2));
     end
     if l > 1
-        disp(sprintf('\tMax. length of interaction list: %d', size(T.interlist,2)));
-        disp(sprintf('\tNumber of unique transfer distances: %d', size(T.D,1)));
+        fprintf(1, '\tMax. length of interaction list: %d\n', size(T.interlist,2));
+        fprintf(1, '\tNumber of unique transfer distances: %d\n', size(T.D,1));
     end
     if l == length(tree)-1
-        disp(sprintf('\tMax. number of source nodes in leaf cluster: %d', size(T.nodsou,2)));
-        disp(sprintf('\tMax. number of receiver nodes in leaf cluster: %d', size(T.nodrec,2)));
+        fprintf(1, '\tMax. number of source nodes in leaf cluster: %d\n', size(T.nodsou,2));
+        fprintf(1, '\tMax. number of receiver nodes in leaf cluster: %d\n', size(T.nodrec,2));
     end
 end
