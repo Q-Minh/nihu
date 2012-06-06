f1bdc0ca584c223e2dd5d9334d6e6c2cdb248771
fiala <fiala@hit.bme.hu>
2012-06-06 15:34:54 +0200

    makeme

---
+++ bem/mex_source/makeme.m
@@ -0,0 +1,16 @@
+make bemHG_const.mex.c
+make bemHG_lin.mex.c
+make ibemB_lin.mex.c
+make ibemD_const.mex.c
+make ibemD_lin.mex.c
+
+make bemHG_const_sp.mex.c
+
+make leafHp.mex.c
+make shiftup.mex.c
+make trans.mex.c
+make recover.mex.c
+make leafGq.mex.c
+make shiftdown.mex.c
+
+% make ibemB_const.mex.c
