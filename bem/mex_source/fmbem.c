f50e347fd0da0998f5b57bbc5665c86396cf0a43
fiala <fiala@hit.bme.hu>
2012-06-08 22:53:09 +0200

    hu

--- bem/mex_source/fmbem.c
+++ bem/mex_source/fmbem.c
@@ -1,24 +1,24 @@
+/* fmbem.c  implementation of the fast multipole bem C routines */
 #include "fmbem.h"
+
 #include "math.h"
 #include "vector.h"
 
-void leafGq(int nnod,				/* numer of nodes */
-            const double *r,		/* node locations */
-            const double *qr,		/* real part of excitation */
-            const double *qi,		/* imaginary part of excitation */
-            const int32_t *father,	/* father cluster indices */
-            int nclus,				/* number of clusters */
-            int ns,					/* number of samples on the unit sphere */
-            const double *s,		/* samples on the unit sphere */
-            double k,				/* wave number */
-            double *Fr,				/* real part of far field signatures */
-            double *Fi)				/* imaginary part of far field signatures */
+/* Compute the leaf level far field signatures of the product Gq */
+void leafGq(int nnod,               /* number of nodes */
+            const double *r,        /* nodal coordinates */
+            const double *qr,       /* real part of nodal velocities */
+            const double *qi,       /* imaginary part of nodal velocities */
+            const int32_t *father,  /* father cluster indices of nodes */
+            int nclus,              /* number of clusters on leaf level */
+            int ns,                 /* number of points on the unit sphere */
+            const double *s,        /* points on the unit sphere */
+            double k,               /* real wave number */
+            double *Fr,             /* real part of far field signarures */
+            double *Fi)             /* imaginary part of far field signarures */
 {
-    int j, p;
-	int32_t f;
-    double phi, cphi, sphi;
-
     /* Clear output matrix */
+    int j, p;
     for (j = 0; j < nclus; j++)
         for (p = 0; p < ns; p++)
             Fr[j*ns+p] = Fi[j*ns+p] = 0.0;
@@ -27,39 +27,37 @@ void leafGq(int nnod,				/* numer of nodes */
     for (j = 0; j < nnod; j++)
     {
         /* father cluster index */
-        f = father[j];
+        int32_t f = father[j];
         /* for each direction (s) */
         for (p = 0; p < ns; p++)
         {
             /* F = q * exp(-i*k*d*s) */
             /* phi = d*s */
-            phi = k * dot(&r[j*3], &s[p*3]);
-            cphi = cos(phi);
-            sphi = sin(phi);
+            double phi = k * dot(&r[j*3], &s[p*3]);
+            double cphi = cos(phi);
+            double sphi = sin(phi);
             Fr[f*ns+p] += qr[j]*cphi+qi[j]*sphi;
             Fi[f*ns+p] += qi[j]*cphi-qr[j]*sphi;
         }
     }
 }
 
+/* Compute the leaf level far field signatures of the product Hp */
 void leafHp(int nnod,
-            const double *r,
-            const double *n,
-            const double *pr,
-            const double *pi,
-            const int32_t *father,
-            int nclus,
-            int ns,
-            const double *s,
-            double k,
-            double *Fr,
-            double *Fi)
+            const double *r,        /* nodal coordinates */
+            const double *n,        /* nodal normals */
+            const double *pr,       /* real part of nodal pressures */
+            const double *pi,       /* imaginary part of nodal pressures */
+            const int32_t *father,  /* father cluster indices of nodes */
+            int nclus,              /* number of clusters on leaf level */
+            int ns,                 /* number of points on the unit sphere */
+            const double *s,        /* points on the unit sphere */
+            double k,               /* real wave number */
+            double *Fr,             /* real part of far field signarures */
+            double *Fi)             /* imaginary part of far field signarures */
 {
-    int i, j, p;
-	int32_t f;
-    double kdvec[3], phi, cphi, sphi, kns;
-
     /* Clear output matrix */
+    int j, p;
     for (j = 0; j < nclus; j++)
         for (p = 0; p < ns; p++)
             Fr[j*ns+p] = Fi[j*ns+p] = 0.0;
@@ -67,50 +65,51 @@ void leafHp(int nnod,
     /* For each source node */
     for (j = 0; j < nnod; j++)
     {
+        int i;
+        double kdvec[3];
         /* father cluster index */
-        f = father[j];
+       	int32_t f = father[j];
         /* distance vector (kd) */
         for (i = 0; i < 3; i++)
             kdvec[i] = k*r[j*3+i];
         /* for each	direction (s) */
         for (p = 0; p < ns; p++)
         {
+            /* phi = d*s */
+            double phi = dot(kdvec, &s[p*3]);
+            double cphi = cos(phi);
+            double sphi = sin(phi);
             /* F = i*k* q * exp(-i*k*d*s) * (n*s) */
             /* k*n*s */
-            kns = k*dot(&n[j*3], &s[p*3]);
-            /* phi = d*s */
-            phi = dot(kdvec, &s[p*3]);
-            cphi = cos(phi);
-            sphi = sin(phi);
+            double kns = k*dot(&n[j*3], &s[p*3]);
             Fr[f*ns+p] -= kns*(pi[j]*cphi-pr[j]*sphi);
             Fi[f*ns+p] += kns*(pr[j]*cphi+pi[j]*sphi);
         }
     }
 }
 
-void recover(int nnod,
-             const double *r,
-             const double *Nr,
-             const double *Ni,
-             const int32_t *father,
-             int ns,
-             const double *s,
-             const double *w,
-             double k,
-             double *pr,
-             double *pi)
+void recover(int nnod,              /* number of nodes */
+             const double *r,       /* nodal distances */
+             const double *Nr,      /* real part of near field signatures */
+             const double *Ni,      /* imaginary part of near field signatures */
+             const int32_t *father, /* father cluster indices */
+             int ns,                /* number of points on the unit sphere */
+             const double *s,       /* points on the unit sphere */
+             const double *w,       /* quadrature weights on the unit sphere */
+             double k,              /* wave number */
+             double *pr,            /* real part of pressure */
+             double *pi)            /* imaginary part of pressure */
 {
-    int i, j, p;
-	int32_t f;
-    double kdvec[3], phi, cphi, sphi;
-
     /* for each receiver node */
+    int j;
     for (j = 0; j < nnod; j++)
     {
+        double kdvec[3];
+        int i, p;
+        /* father cluster of receiver */
+        int32_t f = father[j];
         /* numerical integration initialization */
         pr[j] = pi[j] = 0.0;
-        /* father cluster of receiver */
-        f = father[j];
         /* distance vector ( k*(r-R) ) */
         for (i = 0; i < 3; i++)
             kdvec[i] = -k*(r[j*3+i]);
@@ -118,9 +117,9 @@ void recover(int nnod,
         for (p = 0; p < ns; p++)
         {
             /* phi = k*d*s */
-            phi = dot(kdvec, &s[p*3]);
-            cphi = cos(phi);
-            sphi = sin(phi);
+            double phi = dot(kdvec, &s[p*3]);
+            double cphi = cos(phi);
+            double sphi = sin(phi);
             /* p += N*exp(-phi)*w */
             pr[j] += (Nr[f*ns+p]*cphi+Ni[f*ns+p]*sphi) * w[p];
             pi[j] += (Ni[f*ns+p]*cphi-Nr[f*ns+p]*sphi) * w[p];
@@ -128,62 +127,58 @@ void recover(int nnod,
     }
 }
 
-void upward(int nnod,
-            const double *r,
-            const double *qr,
-            const double *qi,
-            const int32_t *father,
-            int nclus,
-            int ns,
-            const double *s,
-            double k,
-            double *Fr,
-            double *Fi)
+void upward(int nnod,               /* number of nodes */
+            const double *r,        /* nodal distances */
+            const double *qr,       /* real part of signatures */
+            const double *qi,       /* imaginary part of signatures */
+            const int32_t *father,  /* father indices */
+            int nclus,              /* number of clusters */
+            int ns,                 /* number of points on the unit sphere */
+            const double *s,        /* points on the unit sphere */
+            double k,               /* wave number */
+            double *Fr,             /* real part of far field signatures */
+            double *Fi)             /* imaginary part of far field signatures */
 {
-    int i, j, p;
-	int32_t f;
-    double kdvec[3], phi, cphi, sphi;
-
+    int j, p;
     for (j = 0; j < nclus; j++)
         for (p = 0; p < ns; p++)
             Fr[j*ns+p] = Fi[j*ns+p] = 0.0;
 
     for (j = 0; j < nnod; j++)
     {
-        f = father[j];
+        int32_t f = father[j];
+        double kdvec[3];
+        int i;
         for (i = 0; i < 3; i++)
             kdvec[i] = k*r[j*3+i];
         for (p = 0; p < ns; p++)
         {
-            phi = dot(kdvec, &s[3*p]);
-            cphi = cos(phi);
-            sphi = sin(phi);
+            double phi = dot(kdvec, &s[3*p]);
+            double cphi = cos(phi);
+            double sphi = sin(phi);
             Fr[f*ns+p] += qr[j*ns+p]*cphi+qi[j*ns+p]*sphi;
             Fi[f*ns+p] += qi[j*ns+p]*cphi-qr[j*ns+p]*sphi;
         }
     }
 }
 
-void translate(int nclus, /* number of clusters */
-               int ns,    /* number of points on the unit sphere */
-               const double *Fr, /* real part of far field signature */
-               const double *Fi, /* imaginary part of far field signature */
-               int nil, /* max. length of interaction list */
-               const int32_t *I, /* interaction list matrix */
-               const int32_t *D, /* distance matrix */
-               const int32_t *P, /* permutation index matrix */
+void translate(int nclus,           /* number of clusters */
+               int ns,              /* number of points on the unit sphere */
+               const double *Fr,    /* real part of far field signatures */
+               const double *Fi,    /* imaginary part of far field signatures */
+               int nil,             /* max. length of interaction list */
+               const int32_t *I,    /* interaction list matrix */
+               const int32_t *D,    /* distance matrix */
+               const int32_t *P,    /* permutation index matrix */
                const int32_t *Perm, /* permutation matrix */
-               int nm, /* number of translation operators */
-               const double *Mr, /* real part of translation operator matrix */
-               const double *Mi, /* imaginary part of translation operator matrix */
-               double *Nr, /* real part of near field signature matrix */
-               double *Ni) /* imaginary part of near field signature matrix */
+               int nm,              /* number of translation operators */
+               const double *Mr,    /* real part of translation operators */
+               const double *Mi,    /* imaginary part of translation operators */
+               double *Nr,          /* real part of near field signatures */
+               double *Ni)          /* imaginary part of near field signatures */
 {
-    int i, j, p;
-	int32_t Iij, Dij, Pij, pp;
-    double fr, fi, mr, mi;
-
     /* Clear output matrix */
+    int i, p;
     for (i = 0; i < nclus; i++)
         for (p = 0; p < ns; p++)
             Nr[i*ns+p] = Ni[i*ns+p] = 0.0;
@@ -192,27 +187,28 @@ void translate(int nclus, /* number of clusters */
     for (i = 0; i < nclus; i++)
     {
         /* for each receiver cluster in the interaction list */
+        int j;
         for (j = 0; j < nil; j++)
         {
             /* receiver cluster index */
-            Iij = I[i*nil+j];
+            int32_t Iij = I[i*nil+j];
             if (Iij >= 0)
             {
                 /* distance index */
-                Dij = D[i*nil+j];
+                int32_t Dij = D[i*nil+j];
                 /* permutation index */
-                Pij = P[i*nil+j];
+                int32_t Pij = P[i*nil+j];
                 /* for each direction (unit sphere) */
                 for (p = 0; p < ns; p++)
                 {
                     /* permutated index */
-                    pp = Perm[Pij*ns+p];
+                    int32_t pp = Perm[Pij*ns+p];
                     /* source */
-                    fr = Fr[i*ns+p];
-                    fi = Fi[i*ns+p];
+                    double fr = Fr[i*ns+p];
+                    double fi = Fi[i*ns+p];
                     /* translation */
-                    mr = Mr[Dij*ns+pp];
-                    mi = Mi[Dij*ns+pp];
+                    double mr = Mr[Dij*ns+pp];
+                    double mi = Mi[Dij*ns+pp];
                     /* product */
                     Nr[Iij*ns+p] += fr*mr-fi*mi;
                     Ni[Iij*ns+p] += fr*mi+fi*mr;
@@ -222,36 +218,35 @@ void translate(int nclus, /* number of clusters */
     }
 }
 
-void downward(int nnod,
-              const double *r,
-              const double *qr,
-              const double *qi,
-              const int32_t *father,
-              int ns,
-              const double *s,
-              double k,
-              double *Nr,
-              double *Ni)
+void downward(int nnod,             /* number of nodes */
+              const double *r,      /* nodal distances */
+              const double *qr,     /* real part of father signatures */
+              const double *qi,     /* imaginary part of father signatures */
+              const int32_t *father,/* father cluster indices */
+              int ns,               /* number of points on the unit sphere */
+              const double *s,      /* points on the unit sphere */
+              double k,             /* wave number */
+              double *Nr,           /* real part of near field signatures */
+              double *Ni)           /* imaginary part of near field signatures */
 {
-    int i, j, p;
-	int32_t f;
-    double kdvec[3], phi, cphi, sphi;
-
     /* for each receiver cluster */
+    int j;
     for (j = 0; j < nnod; j++)
     {
+        int i, p;
         /* index of father source cluster */
-        f = father[j];
+        int32_t f = father[j];
         /* distance vector (k*(R-r)) */
+        double kdvec[3];
         for (i = 0; i < 3; i++)
             kdvec[i] = -k*r[j*3+i];
         /* for each direction (unit sphere) */
         for (p = 0; p < ns; p++)
         {
             /* phi = kds */
-            phi = dot(kdvec, &s[3*p]);
-            cphi = cos(phi);
-            sphi = sin(phi);
+            double phi = dot(kdvec, &s[3*p]);
+            double cphi = cos(phi);
+            double sphi = sin(phi);
             /* N = q*exp(-kds) */
             Nr[j*ns+p] = qr[f*ns+p]*cphi+qi[f*ns+p]*sphi;
             Ni[j*ns+p] = qi[f*ns+p]*cphi-qr[f*ns+p]*sphi;
--- bem/mex_source/fmbem.h
+++ bem/mex_source/fmbem.h
@@ -1,80 +1,83 @@
+/* fmbem.h  header file for the fast multipole bem C routines */
 #ifndef FMBEM_H
 #define FMBEM_H
 
-#include <stdint.h>
+#include <stdint.h> /* for the definition of int32_t */
 
+/* Compute the leaf level far field signatures of the product Gq */
+void leafGq(int nnod,               /* number of nodes */
+            const double *r,        /* nodal coordinates */
+            const double *qr,       /* real part of nodal velocities */
+            const double *qi,       /* imaginary part of nodal velocities */
+            const int32_t *father,  /* father cluster indices of nodes */
+            int nclus,              /* number of clusters on leaf level */
+            int ns,                 /* number of points on the unit sphere */
+            const double *s,        /* points on the unit sphere */
+            double k,               /* real wave number */
+            double *Fr,             /* real part of far field signarures */
+            double *Fi);            /* imaginary part of far field signarures */
 
-void leafGq(int nnod,
-            const double *r,
-            const double *qr,
-            const double *qi,
-            const int32_t *father,
-            int nclus,
-            int ns,
-            const double *s,
-            double k,
-            double *Fr,
-            double *Fi);
-
+/* Compute the leaf level far field signatures of the product Hp */
 void leafHp(int nnod,
-            const double *r,
-            const double *n,
-            const double *pr,
-            const double *pi,
-            const int32_t *father,
-            int nclus,
-            int ns,
-            const double *s,
-            double k,
-            double *Fr,
-            double *Fi);
+            const double *r,        /* nodal coordinates */
+            const double *n,        /* nodal normals */
+            const double *pr,       /* real part of nodal pressures */
+            const double *pi,       /* imaginary part of nodal pressures */
+            const int32_t *father,  /* father cluster indices of nodes */
+            int nclus,              /* number of clusters on leaf level */
+            int ns,                 /* number of points on the unit sphere */
+            const double *s,        /* points on the unit sphere */
+            double k,               /* real wave number */
+            double *Fr,             /* real part of far field signarures */
+            double *Fi);            /* imaginary part of far field signarures */
 
-void recover(int nnod,
-             const double *r,
-             const double *Nr,
-             const double *Ni,
-             const int32_t *father,
-             int ns,
-             const double *s,
-             const double *w,
-             double k,
-             double *pr,
-             double *pi);
+void recover(int nnod,              /* number of nodes */
+             const double *r,       /* nodal distances */
+             const double *Nr,      /* real part of near field signatures */
+             const double *Ni,      /* imaginary part of near field signatures */
+             const int32_t *father, /* father cluster indices */
+             int ns,                /* number of points on the unit sphere */
+             const double *s,       /* points on the unit sphere */
+             const double *w,       /* quadrature weights on the unit sphere */
+             double k,              /* wave number */
+             double *pr,            /* real part of pressure */
+             double *pi);           /* imaginary part of pressure */
 
-void upward(int nnod,
-            const double *r,
-            const double *qr,
-            const double *qi,
-            const int32_t *father,
-            int nclus,
-            int ns,
-            const double *s,
-            double k,
-            double *Fr,
-            double *Fi);
+void upward(int nnod,               /* number of nodes */
+            const double *r,        /* nodal distances */
+            const double *qr,       /* real part of signatures */
+            const double *qi,       /* imaginary part of signatures */
+            const int32_t *father,  /* father indices */
+            int nclus,              /* number of clusters */
+            int ns,                 /* number of points on the unit sphere */
+            const double *s,        /* points on the unit sphere */
+            double k,               /* wave number */
+            double *Fr,             /* real part of far field signatures */
+            double *Fi);            /* imaginary part of far field signatures */
 
-void translate(int nclus,
-               int ns, const double *Fr,
-               const double *Fi,
-               int nil,
-               const int32_t *I,
-               const int32_t *D,
-               const int32_t *P,
-               const int32_t *Perm,
-               int nm,
-               const double *Mr,
-               const double *Mi,
-               double *Nr,
-               double *Ni);
+void translate(int nclus,           /* number of clusters */
+               int ns,              /* number of points on the unit sphere */
+               const double *Fr,    /* real part of far field signatures */
+               const double *Fi,    /* imaginary part of far field signatures */
+               int nil,             /* max. length of interaction list */
+               const int32_t *I,    /* interaction list matrix */
+               const int32_t *D,    /* distance matrix */
+               const int32_t *P,    /* permutation index matrix */
+               const int32_t *Perm, /* permutation matrix */
+               int nm,              /* number of translation operators */
+               const double *Mr,    /* real part of translation operators */
+               const double *Mi,    /* imaginary part of translation operators */
+               double *Nr,          /* real part of near field signatures */
+               double *Ni);         /* imaginary part of near field signatures */
 
-void downward(int nnod,
-              const double *r,
-              const double *qr,
-              const double *qi,
-              const int32_t *father,
-              int ns,
-              const double *s,
-              double k,
-              double *Nr,
-              double *Ni);
-#endif
+void downward(int nnod,             /* number of nodes */
+              const double *r,      /* nodal distances */
+              const double *qr,     /* real part of father signatures */
+              const double *qi,     /* imaginary part of father signatures */
+              const int32_t *father,/* father cluster indices */
+              int ns,               /* number of points on the unit sphere */
+              const double *s,      /* points on the unit sphere */
+              double k,             /* wave number */
+              double *Nr,           /* real part of near field signatures */
+              double *Ni);          /* imaginary part of near field signatures */
+#endif /* ifndef FMBEM_H */
