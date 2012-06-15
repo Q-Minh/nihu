108ad87dc78c058871ad2ee73c1c5fd1e9b8edc8
fiala <fiala@hit.bme.hu>
2012-06-15 07:29:49 +0200

    2D bem lin started

--- bem/mex_source/bemHG_li.h
+++ bem/mex_source/bemHG_li.h
@@ -18,6 +18,20 @@ void matrix_surf_lin(int nnodes,
                      double *Bi);
 
 /* ------------------------------------------------------------------------ */
+/* Compute surface system matrices of a bem model */
+void matrix_surf_lin2D(int nnodes,
+                     const double *nodes,
+                     int nelements,
+                     const double *elements,
+                     const gauss2D_t *g,
+                     const double *dist,
+                     double k,
+                     double *Ar,
+                     double *Ai,
+                     double *Br,
+                     double *Bi);
+
+/* ------------------------------------------------------------------------ */
 /* Compute far field system matrices of a bem model */
 void matrix_field_lin(int nnodes,
                       const double *nodes,
@@ -33,4 +47,21 @@ void matrix_field_lin(int nnodes,
                       double *Ai,
                       double *Br,
                       double *Bi);
+
+/* ------------------------------------------------------------------------ */
+/* Compute far field system matrices of a bem model */
+void matrix_field_lin2D(int nnodes,
+                      const double *nodes,
+                      int nelements,
+                      const double *elements,
+                      int npoints,
+                      const double *points,
+                      const gauss_t *g,
+                      const double *dist,
+                      double k,
+                      double *Ar,
+                      double *Ai,
+                      double *Br,
+                      double *Bi);
+
 #endif
