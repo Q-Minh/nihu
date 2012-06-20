22a721f3d0670660ffcb13a50cb9b51281199841
Ruster <rucz@hit.bme.hu>
2012-06-20 22:34:05 +0200

    burtonmiller started

--- bem/mex_source/green.c
+++ bem/mex_source/green.c
@@ -27,6 +27,31 @@ void green(const double *r,
     }
 }
 
+/* Compute Green's function and its second derivative (d^2 G / (d nx d ny)) */
+void ddgreen(const double *r,		/* Location */
+	double k, 						/* Wave number */
+	const double* nx, 				/* n_x normal */
+	const double* ny,				/* n_y normal */
+	double *ddgr, 					/* real result */
+	double *ddgi) 					/* imag result */
+{
+	double ar, ar2, ar3, rnx, rny, nxdny, ddr, r1, i1, r2, i2;
+	ar2 = dot(r,r);
+	ar = sqrt(ar2);								/* absolute distance */
+	ar3 = ar2*ar;
+    nxdny = dot(nx, ny); 						/* n_x dot n_y */
+	ddr = dot(r, nx)*dot(r, ny) / ar2;			/* dr / (dn_x)* dr /dn_y */
+	
+	r1 = (3 - k*k*ar2)*ddr + nxdny; 			/* real part in brackets */
+	i1 = (3*k*ar)*ddr + k*ar*nxdny; 			/* imaginary part in brackets */
+	
+	r2 =  cos(k*ar) / ar3 / (4.0 * M_PI);
+	i2 = -sin(k*ar) / ar3 / (4.0 * M_PI);
+	
+	*ddgr = r1*r2 - i1*i2;
+	*ddgi = r1*i2 + i1*r2;
+}
+
 /* ------------------------------------------------------------------------ */
 /* Compute Green's function and its normal derivative                       */
 void green2D(const double *r,
