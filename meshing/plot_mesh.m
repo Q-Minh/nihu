d554dfe76c4cc348e50018067b66b7021dc3866e
fiala <fiala@hit.bme.hu>
2012-09-06 11:36:38 +0200

    incident_kx

--- meshing/plot_mesh.m
+++ meshing/plot_mesh.m
@@ -37,8 +37,8 @@ function plot_mesh(mesh, varargin)
 % Last modified: 30.11.2009.
 
 %% Argument check and default parameters
-error(nargchk(1, 4, nargin, 'struct'));
-error(nargoutchk(0, 0, nargout, 'struct'));
+narginchk(1, 4);
+nargoutchk(0, 0);
 
 switch nargin
     case 1  % PLOT_MESH(MESH);
