6ed325aca02fef9672af723fb3b17568faeaedc0
fiala <fiala@hit.bme.hu>
2012-12-12 11:27:42 +0100

    moving harmonic

--- AcouFEM/AcouFEM.Log.m
+++
@@ -1,3 +0,0 @@
-%% 27-Feb-2012 10:54:34 - AcouFEM started
-data = SInitData;
-data = SNewModel(data, '2.5DCartInf', 0.000000, 1.000000, 10);
--- AcouFEM/SLoadMesh.m
+++
@@ -1,5 +0,0 @@
-function data = SLoadMesh(data, filename)
-
-load(filename, 'Domain');
-data.Model.Domain = Domain;
-data.Model = SInitModel(data.ModelType, data.Model);
