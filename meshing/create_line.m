f09e48f6e4a57f6506c35ccc6c1166fd7cf75c3a
fiala <fiala@hit.bme.hu>
2012-12-12 15:01:08 +0100

    meshing gecire

--- meshing/create_line.m
+++ meshing/create_line.m
@@ -1,5 +1,5 @@
 function model = create_line(varargin)
-%CREATE_LINE  Create a line mesh
+%CREATE_LINE  Create a line mesh (NiHu / meshing)
 %   LINE = CREATE_LINE(L, N) creates a line model with length given
 %   in L and division given in N. The line starts at the origin, and is
 %   aligned along the x axis.
@@ -10,7 +10,7 @@ function model = create_line(varargin)
 %   LINE = CREATE_LINE(Cx) creates a line model aligned along the x axis,
 %   with nodes located at the positions given in vector Cx.
 %
-% See also: CREATE_SLAB, CREATE_CIRCLE, CREATE_CIRCLE_QUADRANT, 
+% See also: CREATE_SLAB, CREATE_CIRCLE, CREATE_CIRCLE_QUADRANT,
 % CREATE_BRICK, CREATE_BRICK_BOUNDARY, CREATE_SPHERE,
 % CREATE_SPHERE_BOUNDARY, CREATE_CATSEYE
 
@@ -24,27 +24,39 @@ function model = create_line(varargin)
 
 % Process input arguments
 switch nargin
+    case 1 % one argument mode
+        Cx = varargin{1};
+        if size(Cx,2) > 3
+            error('NiHu:create_line:argFormat',...
+                'Unsupported format of input arguments.');
+        end
+        N = size(Cx,1) - 1;
     case 2 % Two arguments mode
         if (isscalar(varargin{2})) % L and N are given
-            if (size(varargin{1},1) == 1)
-                R = [0 0 0;
-                     varargin{1}, zeros(1, 3-size(varargin{1},2))];
-            elseif (size(varargin{1},1) == 2)
-                R = [varargin{1}, zeros(2, 3-size(varargin{1},2))];
-            end
+            s = size(varargin{1});
+            R = [zeros(2-s(1), 3);
+                varargin{1}, zeros(1, 3-s(2))];
             N = varargin{2};
         else
             error('NiHu:create_line:argFormat',...
                 'Unsupported format of input arguments.');
         end
     otherwise
-         error('NiHu:create_line:argNumber',...
+        error('NiHu:create_line:argNumber',...
             'Unsupported number of arguments: %d.', nargin);
+end
+if N < 1
+    error('NiHu:create_line:argValue',...
+        'Number of line segments (%d) must be positive', N);
 end
 
 % Transformation
 model = create_line_base(N);            % Create base
-phi = shapefun(model.Nodes(:,2), 12);   % Obtain shape values
-model.Nodes(:,2:4) = phi * R;           % Finish transformation
+if exist('R', 'var')
+    phi = shapefun(model.Nodes(:,2), 12);   % Obtain shape values
+    model.Nodes(:,2:4) = phi * R;           % Finish transformation
+elseif exist('Cx', 'var')
+    model.Nodes(:,1+(1:size(Cx,2))) = Cx;   % replace coordinates
+end
 
 end
