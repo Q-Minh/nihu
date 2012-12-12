6b9f34efbb6fc9e5f78fc730825a7b791b67d6d3
fiala <fiala@hit.bme.hu>
2012-12-12 11:27:42 +0100

    moving harmonic

--- AcouFEM/SFrequencyRange.m
+++
@@ -1,8 +0,0 @@
-function data = SFrequencyRange(data, type, start, stop, steps)
-
-switch lower(type)
-    case 'lin'
-        data.Excitation.FrequencyVector = linspace(start, stop, steps+1);
-    case 'log'
-        data.Excitation.FrequencyVector = logspace(log10(start), log10(stop), steps+1);
-end
--- AcouFEM/SImportMesh.m
+++
@@ -1,6 +0,0 @@
-function data = SImportMesh(data, filename)
-
-Domain = import_bulk_model(filename);
-Domain.Materials = [1 1 1.2 340];
-data.Model.Domain = Domain;
-data.Model = SInitModel(data.ModelType, data.Model);
