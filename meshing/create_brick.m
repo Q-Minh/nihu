e62ec27aa85d6dbc98f6b57c0026e109ac466ba4
fiala <fiala@hit.bme.hu>
2012-12-13 09:47:32 +0100

    vreate_brick

--- meshing/create_brick.m
+++ meshing/create_brick.m
@@ -1,4 +1,4 @@
-function brick = create_brick(varargin)
+function model = create_brick(varargin)
 %CREATE_BRICK  Create a brick volume mesh
 %   BRICK = CREATE_BRICK(L, N) creates a brick model with side lengths
 %   given in the 3D vector L and division given in the 3D vector N.
@@ -21,42 +21,87 @@ function brick = create_brick(varargin)
 % CREATE_CIRCLE_QUADRANT, CREATE_SPHERE, CREATE_SPHERE_BOUNDARY,
 % CREATE_CATSEYE
 
-%   Copyright 2008-2010 P. Fiala
+%   Copyright 2008-2010 P. Fiala, P. Rucz
 %   Budapest University of Technology and Economics
 %   Dept. of Telecommunications
 
-% Last modifed: 02.12.2009
+% Last modifed: 2012.12.13.
 
 %% Parameter check
-if isscalar(L)
-    L = repmat(L,1,3);
-end
-if nargin == 1
-    N = L;
-    Cz = L;
-end
-if isscalar(N)
-    N = repmat(N,1,3);
+switch nargin
+    case 1 % Cx (= Cy = Cz) mode
+        Cx = sort(varargin{1});
+        Cy = Cx;
+        Cz = Cx;
+        N = length(Cx) * [1 1 1] - 1;
+    case 2 % R N mode
+        % Process corners
+        R = varargin{1};
+        switch size(R,1)
+            case 1
+                if isscalar(R)
+                    R = [R R R];
+                end
+                R = [
+                    0    0    0
+                    R(1) 0    0
+                    R(1) R(2) 0
+                    0    R(2) 0
+                    0    0    R(3)
+                    R(1) 0    R(3)
+                    R(1) R(2) R(3)
+                    0    R(2) R(3)
+                    ];
+            case 2
+                R = [
+                    R(1,1) R(1,2) R(1,3)
+                    R(2,1) R(1,2) R(1,3)
+                    R(2,1) R(2,2) R(1,3)
+                    R(1,1) R(2,2) R(1,3)
+                    R(1,1) R(1,2) R(2,3)
+                    R(2,1) R(1,2) R(2,3)
+                    R(2,1) R(2,2) R(2,3)
+                    R(1,1) R(2,2) R(2,3)
+                    ];
+            case 8
+            otherwise
+                error('NiHu:create_brick:argFormat',...
+                    'Unsupported format of input arguments.');
+        end
+        N = varargin{2};
+        if isscalar(N)
+            N = [N N N];
+        end
+    case 3 % Cx Cy Cz mode
+        Cx = sort(varargin{1});
+        Cy = sort(varargin{2});
+        Cz = sort(varargin{3});
+        N = [length(Cx) length(Cy) length(Cz)] - 1;
+    otherwise
+        error('NiHu:create_brick:argNumber',...
+            'Unsupported number of arguments: %d.', nargin);
 end
 
-%% Creating the brick model
-if numel(N) > 3 % Node vectors are given
-    Cx = L;
-    Cy = N;
-    N = [length(Cx) length(Cy) length(Cz)]-1;
-    brick = create_brick(N, N);
-    brick.Nodes(:,2) = Cx(brick.Nodes(:,2)+1);
-    brick.Nodes(:,3) = Cy(brick.Nodes(:,3)+1);
-    brick.Nodes(:,4) = Cz(brick.Nodes(:,4)+1);
-elseif size(L, 1) == 8  % corner nodes are given
-    % uniform centered slab
-    brick = translate_mesh(create_brick(2, N), -[1 1 1]);
-    % node transformation
-    phi = shapefun(brick.Nodes(:,2:4), 38);
-    brick.Nodes(:,2:4) = phi * L;
-else    % side lengths are given
-    brick = extrude_mesh(create_slab(L(1:2), N(1:2)), [0 0 L(3)/N(3)], N(3));
+% Check the processed variables
+if size(N,1) ~= 1 || size(N,2) ~= 3
+    error('NiHu:create_brick:argFormat',...
+        'Unsupported format of input arguments.');
+end
+
+if any(N < 1)
+    error('NiHu:create_brick:argValue',...
+        'Number of segments (%d %d %d) must be positive on each side', N(1), N(2));
+end
+
+% Create base model
+model = create_brick_base(N);
+% Apply transformation
+if exist('R', 'var')
+    phi = shapefun(model.Nodes(:,2:4), 38);
+    model.Nodes(:,2:4) = phi * R;
+elseif exist('Cx', 'var')
+    [z y x] = meshgrid(Cz,Cy,Cx);            % create coordinates
+    model.Nodes(:,2:4) = [x(:) y(:) z(:)];   % replace coordinates
 end
 
 end
-
