328a1416ba7a281ec78a9be7d31c47032776150d
fiala <fiala@hit.bme.hu>
2013-01-23 18:10:06 +0100

    rayleigh test

--- src/bem/rayleigh_test.cpp
+++ src/bem/rayleigh_test.cpp
@@ -1,8 +1,10 @@
 #include "rayleigh.hpp"
 
 typedef tmp::vector<tria_1_elem, quad_1_elem> elem_type_vector;
-typedef rayleigh<elem_type_vector, isoparametric_field> rayleigh_t;
-
+typedef Mesh<elem_type_vector> mesh_t;
+typedef typename mesh_t::x_t x_t;
+typedef rayleigh<elem_type_vector, isoparametric_field> iso_rayleigh_t;
+typedef rayleigh<elem_type_vector, constant_field> const_rayleigh_t;
 
 int main(void)
 {
@@ -25,15 +27,22 @@ int main(void)
 		3,  4, 8, 7, 0
 	};
 
-	Mesh<elem_type_vector> mesh;
+	mesh_t mesh;
 	for (unsigned i = 0; i < sizeof(c)/sizeof(c[0])/3; ++i)
 		mesh.add_node(c+i*3);
 	for (unsigned i = 0; i < sizeof(e)/sizeof(e[0])/5; ++i)
 		mesh.add_elem(e+i*5);
 
-	rayleigh_t r(mesh);
-	auto result = r.eval(rayleigh_t::x_t::Ones(), 1.0);
-	std::cout << "result : " << std::endl << result << std::endl;
+	x_t x0 = x_t::Ones();
+	dcomplex k = 1.0;
+
+	iso_rayleigh_t iso_r(mesh);
+	auto result = iso_r.eval(x0, k);
+	std::cout << "isoparametric result : " << result.sum() << std::endl;
+
+	const_rayleigh_t const_r(mesh);
+	result = const_r.eval(x0, k);
+	std::cout << "constant result : " << result.sum() << std::endl;
 
 	return 0;
 }
