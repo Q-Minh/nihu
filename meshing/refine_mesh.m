a66d3accdc28554fe8fa820134a34b791e157ac7
fiala <fiala@hit.bme.hu>
2013-03-14 06:28:39 +0100

    refine esh general

--- meshing/refine_mesh.m
+++ meshing/refine_mesh.m
@@ -1,37 +1,49 @@
 function newmesh = refine_mesh(mesh)
-%REFINE_MESH refine mesh by dividing its tria and quad elements into subelements
+%REFINE_MESH refine mesh by dividing its elements into subelements
+% NEWMESH = REFINE_MESH(MESH) refines the mesh by dividing its elements
+% into sublements. Division is performed so that the element length is
+% halved.
+%
+% Example:
+%  mesh = create_sphere_boundary(1, 5);
+%  m = refine_mesh(mesh);
+%  figure; plot_mesh(mesh);
+%  figure; plot_mesh(m);
 
 elements = drop_IDs(mesh);
 
-quads = elements(elements(:,2) == 24, :);
-nquad = size(quads,1);
-newquadnodes = zeros(9*nquad,3);
-newquadelements = zeros(4*nquad,4);
-for q = 1 : nquad
-    [newnod, newelem] = refine_quad(mesh.Nodes(quads(q,5:8),2:4));
-    newquadelements((q-1)*4+(1:4),1:4) = newelem + (q-1)*9;
-    newquadnodes((q-1)*9+(1:9),1:3) = newnod;
+elem_data = {
+    12, 2, 3, @refine_line
+    23, 4, 6, @refine_tria
+    24, 4, 9, @refine_quad
+    };
+
+newmesh.Elements = [];
+newmesh.Nodes = [];
+
+for iType = 1 : size(elem_data,1)
+    id = elem_data{iType,1};
+    nelems = elem_data{iType,2};
+    nnodes = elem_data{iType,3};
+    refine_fun = elem_data{iType,4};
+    norigelems = mod(id,10);
+    
+    elems = elements(elements(:,2) == id, :);
+    nel = size(elems,1);
+    newelemnodes = zeros(nnodes*nel,3);
+    newelemelements = zeros(nelems*nel,norigelems);
+    for q = 1 : nel
+        [newnod, newelem] = refine_fun(mesh.Nodes(elems(q,4+(1:norigelems)),2:end));
+        newelemelements((q-1)*nelems+(1:nelems),1:norigelems) = newelem + (q-1)*nnodes;
+        newelemnodes((q-1)*nnodes+(1:nnodes),1:3) = newnod;
+    end
+    
+    newmesh.Elements(end+(1:size(newelemelements,1)),2:(4+norigelems)) =...
+        [repmat([id 1 1], size(newelemelements,1), 1) newelemelements+size(newmesh.Nodes,1)];
+    newmesh.Nodes(end+(1:size(newelemnodes,1)),2:4) = newelemnodes;
 end
 
-trias = elements(elements(:,2) == 23, :);
-ntria = size(trias,1);
-newtrianodes = zeros(9*ntria,3);
-newtriaelements = zeros(4*ntria,3);
-for q = 1 : ntria
-    [newnod, newelem] = refine_tria(mesh.Nodes(trias(q,5:7),2:4));
-    newtriaelements((q-1)*4+(1:4),1:3) = newelem + (q-1)*6;
-    newtrianodes((q-1)*6+(1:6),1:3) = newnod;
-end
-
-
-newmesh.Nodes(:,2:4) = [
-    newquadnodes;
-    newtrianodes;
-    ];
 newmesh.Nodes(:,1) = 1 : size(newmesh.Nodes,1);
-
-newmesh.Elements(1:4*nquad,2:8) = [repmat([24 1 1], 4*nquad, 1) newquadelements];
-newmesh.Elements(end+(1:4*ntria),2:7) = [repmat([23 1 1], 4*ntria, 1) newtriaelements+9*nquad];
 newmesh.Elements(:,1) = 1 : size(newmesh.Elements,1);
 
 newmesh.Properties = mesh.Properties;
@@ -39,6 +51,17 @@ newmesh.Materials = mesh.Materials;
 
 newmesh = merge_coincident_nodes(newmesh);
 
+end
+
+function [nodes, elements] = refine_line(nodes)
+nodes = [
+    nodes
+    mean(nodes,1)
+    ];
+elements = [
+    1 3
+    3 2
+    ];
 end
 
 function [nodes, elements] = refine_tria(nodes)
@@ -58,7 +81,7 @@ function [nodes, elements] = refine_quad(nodes)
 nodes = [
     nodes
     (nodes(1:4,:) + nodes([2 3 4 1], :)) /2
-    sum(nodes,1) / 4
+    mean(nodes,1)
     ];
 elements = [
     1 5 9 8
