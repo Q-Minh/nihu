fe847f187ceaf9ad7a50b2914b2ad130051ba57a
fiala <fiala@hit.bme.hu>
2013-07-12 18:12:47 +0200

    potential bem test

---
+++ src/test/matlab/potential_bem_test.m
@@ -0,0 +1,14 @@
+clear;
+
+surface = create_sphere_boundary(1, 5);
+field = create_sphere_boundary(3, 4);
+
+[ns, es] = extract_Boonen_mesh(surface);
+[nf, ef] = extract_Boonen_mesh(field);
+
+[Gs, Hs, Gf, Hf] = potential_bem(ns, es, nf, ef);
+
+N = size(Gs,1);
+qs = ones(N,1);
+ps = Hs \ (Gs * qs);
+pf = Hf * ps - Gf * qs;
