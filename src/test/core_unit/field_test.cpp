cef6cb1dab8172f1275b29155e646c3652fe8bc3
fiala <fiala@hit.bme.hu>
2013-07-12 09:45:58 +0200

    test modified

--- src/test/core_unit/field_test.cpp
+++ src/test/core_unit/field_test.cpp
@@ -13,19 +13,12 @@ struct view_tester
 			std::cout << std::endl << "Elem type id: " << ElemType::id << std::endl;
 
 			ElemType e(ElemType::coords_t::Random());
-			std::cout << "coords: " << e.get_coords() << std::endl;
 
-			auto cfv = constant_view(e);
-			std::cout << cfv.get_dofs() << std::endl;
+			auto fv = create_field_view(e, option());
+			std::cout << fv.get_dofs() << std::endl;
 			
-			auto ifv = isoparametric_view(e);
-			std::cout << ifv.get_dofs() << std::endl;
-
-			auto dcfv = dirac(cfv);
-			std::cout << dcfv.get_dofs() << std::endl;
-			
-			auto difv = dirac(ifv);
-			std::cout << difv.get_dofs() << std::endl;
+			auto dfv = dirac(fv);
+			std::cout << dfv.get_dofs() << std::endl;
 		}
 	};
 };
@@ -33,15 +26,12 @@ struct view_tester
 int main(void)
 {
 	typedef tmp::vector<tria_1_elem, quad_1_elem, tria_2_elem, quad_2_elem> elem_vector;
+	typedef tmp::vector<field_option::constant, field_option::isoparametric> option_vector;
 
-	tmp::call_each<
+	tmp::d_call_each<
 		elem_vector,
-		view_tester<tmp::_1, field_option::constant>
-	>();
-
-	tmp::call_each<
-		elem_vector,
-		view_tester<tmp::_1, field_option::isoparametric>
+		option_vector,
+		view_tester<tmp::_1, tmp::_2>
 	>();
 
 	return 0;
