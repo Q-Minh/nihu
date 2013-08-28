d3730f30130bd2c3fddf17075c9ef24557b51a9f
fiala <fiala@hit.bme.hu>
2013-08-28 10:44:12 +0200

    single integral restructured a bit

--- src/bem/single_integral.hpp
+++ src/bem/single_integral.hpp
@@ -13,6 +13,19 @@
 #include "gaussian_quadrature.hpp"
 #include "field_type_accelerator.hpp"
 
+template <class TestField, class TrialField>
+struct single_integral_traits
+{
+	/** \brief result type of the single integral residual */
+	typedef typename plain_type<
+		typename product_type<
+			typename TestField::nset_t::shape_t,
+			Eigen::Transpose<typename TrialField::nset_t::shape_t >
+		>::type
+	>::type result_t;
+};
+
+
 /**
 * \brief single integral over an element
 * \tparam TestField type of the test field
@@ -27,6 +40,9 @@ public:
 	/** \brief template parameter as nested type */
 	typedef TrialField trial_field_t;
 
+	typedef single_integral_traits<test_field_t, trial_field_t> traits_t;
+	typedef typename traits_t::result_t result_t;
+
 	/** \brief L-set of the elem */
 	typedef typename test_field_t::elem_t::lset_t lset_t;
 
@@ -37,16 +53,6 @@ public:
 	typedef typename test_field_t::nset_t test_nset_t;
 	/** \brief N-set of the trial field */
 	typedef typename trial_field_t::nset_t trial_nset_t;
-
-	/** \brief result type of the weighted residual */
-	typedef typename plain_type<
-		typename product_type<
-			typename test_nset_t::shape_t,
-			typename plain_type<
-				Eigen::Transpose<typename trial_nset_t::shape_t>
-			>::type
-		>::type
-	>::type result_t;
 
 	/** \brief the polynomial degree of the product of the n-sets and the jacobian */
 	static unsigned const degree
@@ -106,15 +112,9 @@ public:
 	/** \brief N-set of the trial field */
 	typedef typename TrialField::nset_t trial_nset_t;
 
-	/** \brief result type of the weighted residual */
-	typedef typename plain_type<
-		typename product_type<
-			typename  test_nset_t::shape_t,
-			Eigen::Transpose<typename trial_nset_t::shape_t >
-		>::type
-	>::type result_t;
+	typedef single_integral_traits<TestField, TrialField> traits_t;
+	typedef typename traits_t::result_t result_t;
 
-public:
 	/** \brief evaluate collocational integral on a given field
 	* \return integration result by value
 	*/
@@ -136,12 +136,31 @@ public:
 * \tparam TestField type of the test field
 * \tparam TrialField type of the trial field
 */
-template <class TestField, class TrialField>
-class single_integral :
-	public single_integral_impl<field_traits<TestField>::is_dirac, TestField, TrialField>
+template <class TestField, class TrialField, class = void>
+class single_integral
 {
+public:
+	typedef single_integral_traits<TestField, TrialField> traits_t;
+	typedef typename traits_t::result_t result_t;
+
+	static constexpr result_t eval(
+		field_base<TestField> const &,
+		field_base<TrialField> const &)
+	{
+		return result_t::Zero();
+	}
 };
 
+
+template <class TestField, class TrialField>
+class single_integral<TestField, TrialField,
+	typename std::enable_if<
+		std::is_same<
+			typename TestField::elem_t,
+			typename TrialField::elem_t
+		>::type::value
+	>::type
+> :	public single_integral_impl<field_traits<TestField>::is_dirac, TestField, TrialField> {};
 
 #endif // SINGLE_INTEGRAL_HPP
 
