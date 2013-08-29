44c95d81073bfbfe318702712b976dd41fe2d332
fiala <fiala@hit.bme.hu>
2013-08-29 22:26:36 +0200

    tutorial

--- src/tutorial/rayleigh_integral_3d.mex.cpp
+++ src/tutorial/rayleigh_integral_3d.mex.cpp
@@ -12,26 +12,26 @@ void mexFunction(int nlhs, mxArray *lhs[], int nrhs, mxArray const *rhs[])
 //! [Mex function]
 {
 //! [Meshes]
-	dMatrix surf_nodes(rhs[0]), surf_elem(rhs[1]) field_nodes(rhs[2]), field_elem(rhs[3]);
+	dMatrix surf_nodes(rhs[0]), surf_elem(rhs[1]), field_nodes(rhs[2]), field_elem(rhs[3]);
 	auto surface = create_mesh(surf_nodes, surf_elem, _quad_1_tag());
 	auto field = create_mesh(field_nodes, field_elem, _quad_1_tag());
 //! [Meshes]
 
 //! [Function spaces]
-	auto const &trial = isoparametric_view(surface);
-	auto const &surf_test = dirac(constant_view(surface));
-	auto const &field_test = dirac(constant_view(field));
+	auto const &w = isoparametric_view(surface);
+	auto const &dirac_s = dirac(constant_view(surface));
+	auto const &dirac_f = dirac(constant_view(field));
 //! [Function spaces]
 
 //! [Kernel and weighted residual]
 	double wave_number = *mxGetPr(rhs[4]);
-	auto K = create_integral_operator(helmholtz_3d_SLP_kernel<double>(wave_number));
+	auto G = create_integral_operator(helmholtz_3d_SLP_kernel<double>(wave_number));
 
-	cMatrix Z_trans(field_test.get_num_dofs(), trial.get_num_dofs(), lhs[0]);
-	cMatrix Z_rad(surf_test.get_num_dofs(), trial.get_num_dofs(), lhs[1]);
+	cMatrix Z_rad(dirac_s.get_num_dofs(), w.get_num_dofs(), lhs[1]);
+	cMatrix Z_trans(dirac_f.get_num_dofs(), w.get_num_dofs(), lhs[0]);
 
-	Z_trans << (field_test * K[trial]);
-	Z_rad << (surf_test * K[trial]);
+	Z_rad << (dirac_s * G[w]);
+	Z_trans << (dirac_f * G[w]);
 //! [Kernel and weighted residual]
 }
 
