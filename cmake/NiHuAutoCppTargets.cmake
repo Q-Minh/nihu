9878737087ee09d5f9e3d77c2d9331913adbdae5
Ruster <rucz@hit.bme.hu>
2013-08-29 17:16:43 +0200

    auto mex targets

--- cmake/NiHuAutoCppTargets.cmake
+++ cmake/NiHuAutoCppTargets.cmake
@@ -9,8 +9,47 @@ foreach (cpp_source ${CPP_SOURCES})
 	file(RELATIVE_PATH local_source ${CMAKE_CURRENT_SOURCE_DIR} ${cpp_source})
 	# Construct test name based on file name
 	string(REPLACE ".cpp" "" target_name ${local_source})
-	# Add the test executable
-	add_executable(${target_name} ${local_source})
-	# Add installation
-	install(TARGETS ${target_name} DESTINATION ${current_dir})
+	string(REPLACE ".mex" "" target_mex_name ${target_name})
+
+	# Add the executable / or mex file
+	if(${target_mex_name} MATCHES ${target_name})
+		add_executable(${target_name} ${local_source})
+		# Add installation
+		install(TARGETS ${target_name} DESTINATION ${current_dir})
+	else()
+		# Do not use the mex compiler
+		if(NOT NIHU_FORCE_MEX_COMPILER)
+			# add the test as a shared library
+			add_library(${target_mex_name} SHARED ${local_source})
+			# remove the "lib" prefix
+			set_target_properties(${target_mex_name} PROPERTIES 
+				PREFIX "" 
+				SUFFIX "${MATLAB_MEXEXT}"
+				COMPILE_FLAGS "${MEX_CXX_FLAGS}"
+				LINK_FLAGS "${MEX_SHARED_LINKER_FLAGS}"
+			)
+		# Use the mex compiler
+		else(NOT NIHU_FORCE_MEX_COMPILER)
+			ADD_CUSTOM_TARGET (${target_mex_name} ALL)
+			ADD_CUSTOM_COMMAND(
+				TARGET    ${target_mex_name}
+				COMMAND   ${MATLAB_MEX}
+				ARGS      -O 
+					CXXFLAGS=\""${CMAKE_CXX_FLAGS} -fPIC"\"
+					-I"${MATLAB_ROOT}/extern/include" 
+					-I"${EIGEN_INCLUDE_DIR}"
+					-I"${CMAKE_SOURCE_DIR}" 
+					-I"${CMAKE_SOURCE_DIR}/util" 
+					"${CMAKE_CURRENT_SOURCE_DIR}/${local_source}"
+					-o "${test_name}"
+				COMMENT "Executing MEX for ${local_source}"
+			)
+		endif(NOT NIHU_FORCE_MEX_COMPILER)
+
+		# Add installation
+		install(TARGETS ${target_mex_name} DESTINATION ${current_dir})
+	endif()
+
+	
+	
 endforeach(cpp_source)
--- src/tutorial/rayleigh_integral_3d.mex.cpp
+++ src/tutorial/rayleigh_integral_3d.mex.cpp
@@ -14,13 +14,13 @@ void mexFunction(int nlhs, mxArray *lhs[], int nrhs, mxArray const *rhs[])
 //! [Surface mesh]
 	auto surf_nodes = dMatrix(rhs[0]);
 	auto surf_elem = dMatrix(rhs[1]);
-	auto surface = create_mesh(surf_nodes, surf_elements, _quad_1_tag());
+	auto surface = create_mesh(surf_nodes, surf_elem, _quad_1_tag());
 //! [Surface mesh]
 
 //! [Field point mesh]
 	auto field_nodes = dMatrix(rhs[2]);
 	auto field_elem = dMatrix(rhs[3]);
-	auto field = create_mesh(field_nodes, field_elements, _quad_1_tag());
+	auto field = create_mesh(field_nodes, field_elem, _quad_1_tag());
 //! [Field point mesh]
 	
 //! [Function spaces]
@@ -29,7 +29,7 @@ void mexFunction(int nlhs, mxArray *lhs[], int nrhs, mxArray const *rhs[])
 //! [Function spaces]
 
 //! [Kernel and weighted residual]
-	double wave_number = *mxRealData(rhs[4]);
+	double wave_number = *mxGetPr(rhs[4]);
 	auto K = create_integral_operator(helmholtz_3d_SLP_kernel<double>(wave_number));
 	cMatrix Z(test.get_num_dofs(), trial.get_num_dofs(), lhs[0]);
 	Z << (test * K[trial]);
