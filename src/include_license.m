102187ea76da6d9e32c00a2807d8fa7b51ea205f
fiala <fiala@hit.bme.hu>
2013-09-12 14:45:23 +0200

    license generator files added

---
+++ src/include_license.m
@@ -0,0 +1,44 @@
+function include_license(Insert, root)
+if nargin == 1
+    root = pwd();
+end
+
+files = dir(fullfile(root, '*.*pp'));
+
+for i = 1 : length(files)
+    fprintf(1, 'processing file %s\n', fullfile(root, files(i).name));
+    fid = fopen(fullfile(root, files(i).name), 'rt');
+    content = textscan(fid, '%s', 'delimiter', '\n', 'WhiteSpace', '');
+    content = content{1};
+    fclose(fid);
+    
+    if (strcmp(Insert{1}, content{1}))
+        continue;
+    end
+    
+    content = [
+        Insert
+        {''}
+        content
+        ];
+    
+    fid = fopen(fullfile(root, files(i).name), 'wt');
+    for l = 1 : length(content);
+        fprintf(fid, '%s\n', content{l});
+    end
+    fclose(fid);
+end
+
+exclude = {'.', '..', 'ThirdParty'};
+
+dirs = dir(fullfile(root));
+dirs = dirs(cell2mat({dirs.isdir}));
+for i = 1 : length(dirs)
+    if ismember(dirs(i).name, exclude)
+        continue;
+    end
+    
+    include_license(Insert, fullfile(root, dirs(i).name));
+end
+
+end
