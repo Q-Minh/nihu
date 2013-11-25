967142f0e2a9815a70ffebdf8dc085ab2f02c8bd
fiala <fiala@hit.bme.hu>
2013-11-25 18:24:41 +0100

    Oltean test

--- src/test/matlab/acoustic_2d_bem_test.m
+++ src/test/matlab/acoustic_2d_bem_test.m
@@ -1,17 +1,17 @@
 clear
 R0 = 1;
 R1 = 2;
-N = 100;
+N = 200;
 
 dir = [1 1 0];
-
-radiator = create_circle_boundary(R0, N);
-[x, n] = centnorm(radiator);
 dir = dir / norm(dir);
 
+radiator = create_circle_boundary(R0, N);
 field_mesh = create_circle_boundary(R1, N);
+[xs, ns] = centnorm(radiator);
+[xf, nf] = centnorm(field_mesh);
 
-k = .5*mesh_kmax(radiator);
+k = 2;
 
 [r_nodes, r_elements] = extract_core_mesh(radiator);
 [f_nodes, f_elements] = extract_core_mesh(field_mesh);
@@ -20,10 +20,20 @@ tic;
 [Ls, Ms, Lf, Mf] = acoustic_2d_bem(r_nodes, r_elements, f_nodes, f_elements, k);
 t_Booni = toc;
 
-pinc = exp(-1i*k*(x*dir'));
-qinc = -1i*k*(n*dir').*pinc;
+pinc_s = exp(-1i*k*(xs*dir'));
+qinc_s = -1i*k*(ns*dir').*pinc_s;
+pinc_f = exp(-1i*k*(xf*dir'));
 
-qref = -qinc;
-pref = Ms \ (Ls * qref);
+qref_s = -qinc_s;
+pref_s = Ms \ (Ls * qref_s);
+ptot_s = pinc_s + pref_s;
 
-ptot = pinc + pref;
+pref_f = Mf * pref_s - Lf * qref_s;
+ptot_f = pinc_f + pref_f;
+
+rf = sqrt(dot(xf,xf,2));
+phif = atan2(xf(:,2), xf(:,1));
+ptot_f_anal = planewave_cyl2d(rf, phif, R0, k, 30);
+
+plot(atan2(xf(:,2), xf(:,1)), abs(ptot_s), '.', ...
+    atan2(xf(:,2), xf(:,1)), abs(ptot_f_anal), '.');
