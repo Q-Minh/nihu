cae256d6678440dcf36b5e5a1f2ba50292e4c83b
fiala <fiala@hit.bme.hu>
2013-11-26 22:07:18 +0100

    oltean revised

--- src/matlab/analytic/planewave_cyl2d.m
+++ src/matlab/analytic/planewave_cyl2d.m
@@ -9,18 +9,15 @@ function ps = planewave_cyl2d(x, a, k, m)
 
 [phi, r] = cart2pol(x(:,1), x(:,2));
 
-mm = 1:m;
-ps = zeros(length(r),m+1);
+g = [
+    atan(-besselj(1,k*a)/bessely(1,k*a)), ...
+    atan((besselj((1:m)-1,k*a)-besselj((1:m)+1,k*a))./(bessely((1:m)+1,k*a)-bessely((1:m)-1,k*a)))
+    ];
+A = -1i*[1, 2*1i.^(1:m)].*exp(-1i*g).*sin(g);
 
-g0 = atan(-besselj(1,k*a)/bessely(1,k*a));	
-A0 = -1i*exp(-1i*g0)*sin(g0);
-ps(:,1) = repmat(A0,length(r),1).*(besselj(0,k*r)+1i*bessely(0,k*r));
-
-gm = atan((besselj(mm-1,k*a)-besselj(mm+1,k*a))./(bessely(mm+1,k*a)-bessely(mm-1,k*a)));
-Am = -2*1i.^(mm+1).*exp(-1i*gm).*sin(gm);
-ps(:,2:end) = repmat(Am,length(r),1).*cos(phi*mm).*...
-    (besselj(repmat(mm, length(r),1),repmat(k*r, 1, m))+...
-    1i*bessely(repmat(mm, length(r), 1),repmat(k*r, 1, m)));
+ps = repmat(A,length(r),1).*cos(phi*(0:m)).*...
+    besselh(repmat(0:m, length(r),1),...
+    ones(length(r), m+1), repmat(k*r, 1, m+1));
 
 ps = conj(sum(ps,2));
 
