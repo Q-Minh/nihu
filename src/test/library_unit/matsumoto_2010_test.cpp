fafa688cd986fd1e376a4f12db6bc1968fde71ac
fiala <fiala@hit.bme.hu>
2013-12-05 17:36:04 +0100

    geexcirejao

--- src/test/library_unit/matsumoto_2010_test.cpp
+++ src/test/library_unit/matsumoto_2010_test.cpp
@@ -16,21 +16,20 @@ typedef Eigen::Matrix<unsigned, Eigen::Dynamic, Eigen::Dynamic> uMatrix;
 int main()
 {
 	// nodal coordinates in 3D, 3 nodes
-	dMatrix nodes(3,3);
+	dMatrix nodes(3, 3);
 	nodes <<
 		-1.0, -1.0, 0.0,
-		 0.0, -1.0, 0.0,
-		 0.0,  0.0, 0.0;
-		 
+		0.0, -1.0, 0.0,
+		0.0, 0.0, 0.0;
+
 	// element nodal indices
-	uMatrix elements(1, 1+4);
+	uMatrix elements(1, 1 + 4);
 	elements <<
 		tria_1_elem::id, 0, 1, 2, 0;
-		
-		
+
 	// create the mesh
 	auto msh = create_mesh(nodes, elements, tria_1_tag());
-	
+
 	// create a piecewise constant function space
 	auto const &w = constant_view(msh);
 
@@ -38,19 +37,21 @@ int main()
 	int nDOF = w.get_num_dofs();
 	cMatrix H_mat(nDOF, nDOF);
 	H_mat.setZero();
-	
+
 	cMatrix G_mat(nDOF, nDOF);
 	G_mat.setZero();
-	
+
+	double k = 0.05;
+
 	// create integral operator from kernel and perform weighted double integral
-	auto H = create_integral_operator(helmholtz_3d_HSP_kernel<double>(0.05));
-	H_mat <<  dirac(w) * H[w];
-		
-	auto G = create_integral_operator(helmholtz_3d_SLP_kernel<double>(0.05));
-	G_mat <<  dirac(w) * G[w];
-	
+	auto H = create_integral_operator(helmholtz_3d_HSP_kernel<double>(k));
+	H_mat << dirac(w) * H[w];
+
+	auto G = create_integral_operator(helmholtz_3d_SLP_kernel<double>(k));
+	G_mat << dirac(w) * G[w];
+
 	std::cout << "H = " << std::endl << H_mat << std::endl << std::endl;
 	std::cout << "G = " << std::endl << G_mat << std::endl << std::endl;
-	
+
 	return 0;
 }
