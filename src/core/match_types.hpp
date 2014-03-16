5d3ddafcf3edaf8a498134245c76d6aca3501f2e
fiala <fiala@hit.bme.hu>
2014-03-16 09:47:43 +0100

    match test better

--- src/core/match_types.hpp
+++ src/core/match_types.hpp
@@ -1,12 +1,23 @@
 /** \file match_types.hpp */
 
+#ifndef MATCH_TYPES_HPP_INCLUDED
+#define MATCH_TYPES_HPP_INCLUDED
+
 #include <type_traits>
 #include "formalism.hpp"
 #include "element_match.hpp"
+#include "../tmp/algorithm.hpp"
 
+/** \brief matafunction assigning a match type vector to two fields
+ * \details The match type vector is a compile time vector containing
+ * possible match dimensions.
+ */
 template <class TestField, class TrialField, class Enable = void>
 struct match_type_vector;
 
+/** \brief specialisation of match_type_vector for the general formalism
+ *  \todo incorporate that a d-match is only possible if the element domains are the same
+ */
 template <class TestField, class TrialField>
 struct match_type_vector<TestField, TrialField,
 	typename std::enable_if<
@@ -17,8 +28,10 @@ struct match_type_vector<TestField, TrialField,
 	>::type
 >
 {
+private:
 	enum { d = TestField::elem_t::domain_t::dimension };
 
+public:
 	typedef typename std::conditional<
 		d == 2,
 		tmp::vector<match::match_0d_type, match::match_1d_type, match::match_2d_type>,
@@ -34,6 +47,9 @@ struct match_type_vector<TestField, TrialField,
 	>::type type;
 };
 
+/** \brief specialisation of match_type_vector for the collocational formalism
+ * \todo incorporate that a d-match is only possible if the element domains are the same
+ */
 template <class TestField, class TrialField>
 struct match_type_vector<TestField, TrialField,
 	typename std::enable_if<
@@ -44,10 +60,13 @@ struct match_type_vector<TestField, TrialField,
 	>::type
 >
 {
+private:
 	enum { d = TestField::elem_t::domain_t::dimension };
 
+	// start with empty vector
 	typedef tmp::vector<> t;
 
+	// push back 0d if needed
 	typedef typename std::conditional<
 		tmp::is_member<typename shape_set_traits::position_dof_vector<typename TestField::nset_t>::type, dof0>::value
 		&&
@@ -56,6 +75,7 @@ struct match_type_vector<TestField, TrialField,
 		t
 	>::type type0;
 
+	// push back 0d if needed
 	typedef typename std::conditional<
 		tmp::is_member<typename shape_set_traits::position_dof_vector<typename TestField::nset_t>::type, dof1>::value
 		&&
@@ -64,6 +84,7 @@ struct match_type_vector<TestField, TrialField,
 		type0
 	>::type type1;
 
+	// push back 2d if needed
 	typedef typename std::conditional<
 		tmp::is_member<typename shape_set_traits::position_dof_vector<typename TestField::nset_t>::type, dof2>::value
 		&&
@@ -72,6 +93,9 @@ struct match_type_vector<TestField, TrialField,
 		type1
 	>::type type2;
 
+public:
 	typedef type2 type;
 };
+
+#endif // MATCH_TYPES_HPP_INCLUDED
 
