5487b0dcc1902c4b7603e14e72c46fc14d2f87a4
Fiala Petike <fiala@hit.bme.hu>
2014-08-15 20:29:07 +0200

    cleared

--- bem/bempow.m
+++
@@ -1,7 +0,0 @@
-function P = bempow(model, p, v)
-
-if length(p) == size(model.Elements,1)
-    [gcoord, gnorm, w] = geo2gauss(model, 1);
-    I = .5 * real(p .* conj(v));
-    P = I .* repmat(w, 1, size(I,2));
-end
--- meshing/create_empty_mesh.m
+++
@@ -1,9 +0,0 @@
-function mesh = create_empty_mesh
-%CREATE_EMPTY_MESH Create an empty NiHu mesh (NiHu / meshing)
-
-mesh.Nodes = zeros(0,4);
-mesh.Elements = zeros(0,6);
-[mesh.Materials, mesh.Properties] = default_mat_prop();
-
-end
-
