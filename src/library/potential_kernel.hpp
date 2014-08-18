de918ef32e5fb1820a68ad754667bafdf05974a9
Fiala Petike <fiala@hit.bme.hu>
2014-08-18 21:49:13 +0200

    potential_kernel.hpp added

---
+++ src/library/potential_kernel.hpp
@@ -0,0 +1,15 @@
+#ifndef POTENTIAL_KERNEL_HPP_INCLUDED
+#define POTENTIAL_KERNEL_HPP_INCLUDED
+
+namespace potential
+{
+	/** \brief tag type to identify the Single Layer Potential */
+	struct SLP { typedef SLP type; };
+	/** \brief tag type to identify the Double Layer Potential */
+	struct DLP { typedef DLP type; };
+	/** \brief tag type to identify the Hypersingular Potential */
+	struct HSP { typedef HSP type; };
+}
+
+#endif // POTENTIAL_KERNEL_HPP_INCLUDED
+
