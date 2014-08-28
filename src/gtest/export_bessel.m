481e4a663a33264efa8657eb15235ea17260b6b3
fiala <fiala@hit.bme.hu>
2014-08-28 19:36:25 +0200

    new Bessel function data for testing

--- src/gtest/export_bessel.m
+++ src/gtest/export_bessel.m
@@ -1,14 +1,31 @@
-clear;
+%EXPORT_BESSEL export Bessel function values in C arrays for testing
 
-r = logspace(-4, 2, 7);
-phi = (-5 : 15 : 360) / 180* pi;
-[R, PHI] = meshgrid(r, phi);
-Z = R .* exp(1i*PHI);
+r = logspace(-5, 4, 15);				% modulus
+phi = (0 : 1 : 360) / 180* pi;	% angle
+Z = bsxfun(@times, r, exp(1i*phi'));	% complex arguments
 
-D = bessely(1, Z);
+functions = {
+	'J0', @(z)besselj(0,z),
+	'J1', @(z)besselj(1,z),
+	'Y0', @(z)bessely(0,z),
+	'Y1', @(z)bessely(1,z),
+	'K0', @(z)besselk(0,z),
+	'K1', @(z)besselk(1,z),
+	'H01', @(z)besselh(0,1,z),
+	'H11', @(z)besselh(1,1,z),
+	'H02', @(z)besselh(0,2,z),
+	'H12', @(z)besselh(1,2,z)
+};
 
-fid = fopen('data/besselY1.dat', 'wt');
-fprintf(fid, 'double besselY1[][4] = {\n');
-fprintf(fid, '\t{%.10g, %.10g, %.10g, %.10g},\n', [real(Z(:)), imag(Z(:)), real(D(:)), imag(D(:))].');
-fprintf(fid, '};\n');
-fclose(fid);
+for i = 1 : size(functions,1)
+	fun = functions{i,2};
+	D = fun(Z);
+    idx = ~isinf(D) & ~isnan(D) & abs(D) < 1e100 &abs(D) > 1e-100;
+	name = functions {i,1};
+
+	fid = fopen(fullfile('data', sprintf('bessel%s.dat', name)), 'wt');
+	fprintf(fid, 'double bessel%s[][4] = {\n', name);
+	fprintf(fid, '\t{%.10g, %.10g, %.10g, %.10g},\n', [real(Z(idx)), imag(Z(idx)), real(D(idx)), imag(D(idx))].');
+	fprintf(fid, '};\n');
+	fclose(fid);
+end
