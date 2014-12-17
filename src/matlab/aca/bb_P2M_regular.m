5e613ef4890d53fca345bab55f54be2ab1d887f6
fiala <fiala@hit.bme.hu>
2014-12-17 09:06:02 +0100

    merged

--- src/matlab/aca/bb_P2M_regular.m
+++ src/matlab/aca/bb_P2M_regular.m
@@ -7,31 +7,9 @@ nPts = length(fathersou);
 dim = size(dsrc,2);
 N = nExp^dim;
 
-<<<<<<< HEAD
-II = zeros(nPts*N,1);
-JJ = zeros(nPts*N,1);
-KK = zeros(nPts*N,1);
-n = 0;
-for iClus = 1 : nClus
-    progbar(1, nClus, iClus);
-    idx = (iClus-1)*N + (1:N);
-    s = find(fathersou == iClus);
-    xs = dsrc(s,:);
-    Z = chebinterp(nExp, xs);
-    [J, I] = meshgrid(s, idx);
-    y = n+(1:numel(J));
-    II(y) = I(:);
-    JJ(y) = J(:);
-    KK(y) = Z(:);
-    n = y(end);
-end
-
-P2M = sparse(II, JJ, KK, N*nClus, nPts);
-=======
 Z = chebinterp(nExp, dsrc);
 II = bsxfun(@plus, (1:N)', N*(fathersou'-1));
 JJ = repmat(1:nPts, N, 1);
 P2M = sparse(II(:), JJ(:), Z(:), N*nClus, nPts);
->>>>>>> b95d07095cd9dfb87b6356d922e81e6e58e3d4f8
 
 end
