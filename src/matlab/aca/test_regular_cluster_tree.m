bc3709b7389c31859346ffd6035cdb377211924c
Fiala Petike <fiala@hit.bme.hu>
2014-12-19 22:20:01 +0100

    plot_clusters

--- src/matlab/aca/test_regular_cluster_tree.m
+++ src/matlab/aca/test_regular_cluster_tree.m
@@ -4,13 +4,14 @@ clear;
 profile on
 
 %%
-dim = 3;
+dim = 2;
 
 if dim == 3
     msh = create_sphere_boundary(1, 105);
     depth = 7;
 elseif dim == 2
-    msh = create_circle(1, 30);
+    msh = create_circle(1, 70);
+    depth = 6;
 end
 xc = centnorm(msh);
 xc = xc(:,1:dim);
@@ -19,20 +20,20 @@ xc = xc(:,1:dim);
 %%
 kernel = @laplace_kernel;
 kernel_sp = @laplace_kernel_sp;
-nExp = 4;
+nExp = 5;
 P2P = bb_P2P_regular(T(end), xc, xc, kernel_sp);
 P2M = bb_P2M_regular(nExp, size(T(end).coord,1), fathersou, dsrc);
 M2M = bb_M2M_regular(dim, nExp);
 M2L = bb_M2L_regular(dim, kernel, nExp);
 L2L = bb_L2L_regular(dim, nExp);
 
-eps = 1e-4;
-[CM2L.U, CM2L.V, CM2L.r] = compress_M2L(M2L, eps);
+% eps = 1e-4;
+% [CM2L.U, CM2L.V, CM2L.r] = compress_M2L(M2L, eps);
 
 %%
-matfun = @(x)bb_matvec_regular(x, T, P2P, P2M, M2M, CM2L, L2L, P2M.', -1);
+matfun = @(x)bb_matvec_regular(x, T, P2P, P2M, M2M, M2L, L2L, P2M.', -1);
 L = .2;
-sigma0 = sin(xc(:,1)*pi/L);
+sigma0 = xc(:,1);
 fi0 = matfun(sigma0);
 [sigma,flag,relres,iter,resvec] = gmres(matfun, fi0);
 
