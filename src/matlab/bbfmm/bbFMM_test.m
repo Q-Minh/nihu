81325fd8e24c1718f44ea8abc9556b68b30246a2
Peter Fiala <fiala@hit.bme.hu>
2016-09-29 10:18:13 +0200

    variable naming in bbfmm

--- src/matlab/bbfmm/bbFMM_test.m
+++ src/matlab/bbfmm/bbFMM_test.m
@@ -1,36 +1,46 @@
 clear;
 clc;
 
-K = @(x,y)1./abs(bsxfun(@minus, x(:), y(:).'));
+K = @(x,y)laplace_kernel(x,y);
 
 %%
 Nx = 30;        % number of points in X interval
 X = [1 2]';      % X interval
 Ny = 20;        % number of points in Y interval
 Y = [3 4]';      % Y interval
-n = 4;          % expansion length
+nCheb = 8;          % expansion length
 
 % generate points
-x = linspace(X(1), X(2), Nx)';
-y = linspace(Y(1), Y(2), Ny)';
+x = linspace(X(1), X(2), Nx)';  % receivers
+y = linspace(Y(1), Y(2), Ny)';  % sources
 
 %%
-P2M = chebinterp(n, y, Y);
-M2L = K(lintrans(chebroots(n), [-1 1]', X),...
-    lintrans(chebroots(n), [-1 1]', Y));
-L2P = chebinterp(n, x, X)';
+P2M = chebinterp(nCheb, y, Y);
+xNodes = lintrans(chebroots(nCheb), [-1 1]', X);
+yNodes = lintrans(chebroots(nCheb), [-1 1]', Y);
+M2L = K(xNodes, yNodes);
+L2P = chebinterp(nCheb, x, X)';
 
 %%
 sigma = rand(Ny,1); % excitation points
 
-Far = P2M*sigma;
-Near = M2L * Far;
-f = L2P * Near;
+%% fmm
+Multi = P2M*sigma;  % anterpolation
+Local = M2L * Multi; % kernel evaluation
+f = L2P * Local; % interpolation
 
+%% conventional direct summation
 f0 = K(x, y) * sigma;
 
+%% plot results and check error
 figure;
+formatfig;
+subplot(2,1,1);
 plot(x, f, x, f0);
-figure;
+legend('fmm', 'conventional', 'location', 'northwest');
+xlabel('x');
+subplot(2,1,2);
 plot(x, abs(f-f0)./abs(f0))
+xlabel('x');
+legend('error');
 
--- src/matlab/bbfmm/bbmultilevelFMM_test.m
+++ src/matlab/bbfmm/bbmultilevelFMM_test.m
@@ -1,6 +1,6 @@
 clear;
 
-kernel = @(x,y)1./abs(bsxfun(@minus, x(:), y(:)'));
+kernel = @(x,y)laplace_kernel(x,y);
 
 %%
 interval = [0 20]';
@@ -8,21 +8,23 @@ D = diff(interval); % geometry size
 % Nx points in first third
 Nx = 1e4;
 x = linspace(interval(1,1), interval(2,1)/3, Nx+2)';
-x = x(2:end-1);
+x = x(2:end-1); % drop end points
 % Ny points in 3rd third
 Ny = 1e4;
 y = linspace(interval(2)*2/3, interval(2), Ny+2)';
 y = y(2:end-1);
 
-nExp = 5;          % expansion length of Cheby interpolation
+nCheb = 5;          % expansion length of Cheby interpolation
 
 %% build cluster tree
 nLeaf = 10;         % maximal number of particles on leaf level
 depth = round(log2(max(Nx, Ny)/nLeaf));
-[tree, m2m] = build_tree(D, depth, nExp, kernel);
+[tree, m2m] = build_tree(D, depth, nCheb, kernel);
 
 %% build leaf contribution sparse matrices
-[P2M, L2P] = leaf_contribution(x, y, tree(end), nExp);
+fprintf(1, 'Computing P2M and L2P...');
+[P2M, L2P] = leaf_contribution(x, y, tree(end), nCheb);
+fprintf(1, 'done\n');
 
 %%
 sigma = rand(Ny,1);
@@ -31,12 +33,12 @@ Multipole = P2M * sigma;
 for level = depth : -1 : 2
     tree(level).Local = tree(level).M2L * Multipole;
     if level > 2
-        Multipole = reshape(m2m * reshape(Multipole, 2*nExp, []), [], 1);
+        Multipole = reshape(m2m * reshape(Multipole, 2*nCheb, []), [], 1);
     end
 end
 for level = 3 : depth
     tree(level).Local = tree(level).Local +...
-        reshape(m2m' * reshape(tree(level-1).Local, nExp, []), [], 1);
+        reshape(m2m' * reshape(tree(level-1).Local, nCheb, []), [], 1);
 end
 res = L2P * tree(end).Local;
 tfmm = toc;
