2e52a9284538596265d569f698f2d412ec6568f2
Peter Fiala <fiala@hit.bme.hu>
2017-02-14 10:56:08 +0100

    new files added

--- src/gtest/test_bessel.cpp
+++ src/gtest/test_bessel.cpp
@@ -20,51 +20,51 @@ void bessel_tester(double data[][4], unsigned size, std::complex<double> (*func)
 
 TEST(Bessel, J0) {
 	#include "data/besselJ0.dat"
-	bessel_tester(besselJ0, sizeof(besselJ0)/sizeof(besselJ0[0]), bessel::J<0>);
+	bessel_tester(besselJ0, sizeof(besselJ0)/sizeof(besselJ0[0]), NiHu::bessel::J<0>);
 }
 
 TEST(Bessel, J1) {
 	#include "data/besselJ1.dat"
-	bessel_tester(besselJ1, sizeof(besselJ1)/sizeof(besselJ1[0]), bessel::J<1>);
+	bessel_tester(besselJ1, sizeof(besselJ1)/sizeof(besselJ1[0]), NiHu::bessel::J<1>);
 }
 
 TEST(Bessel, Y0) {
 	#include "data/besselY0.dat"
-	bessel_tester(besselY0, sizeof(besselY0)/sizeof(besselY0[0]), bessel::Y<0>);
+	bessel_tester(besselY0, sizeof(besselY0)/sizeof(besselY0[0]), NiHu::bessel::Y<0>);
 }
 
 TEST(Bessel, Y1) {
 	#include "data/besselY1.dat"
-	bessel_tester(besselY1, sizeof(besselY1)/sizeof(besselY1[0]), bessel::Y<1>);
+	bessel_tester(besselY1, sizeof(besselY1)/sizeof(besselY1[0]), NiHu::bessel::Y<1>);
 }
 
 TEST(Bessel, H01) {
 	#include "data/besselH01.dat"
-	bessel_tester(besselH01, sizeof(besselH01)/sizeof(besselH01[0]), bessel::H<0,1>);
+	bessel_tester(besselH01, sizeof(besselH01)/sizeof(besselH01[0]), NiHu::bessel::H<0,1>);
 }
 
 TEST(Bessel, H02) {
 	#include "data/besselH02.dat"
-	bessel_tester(besselH02, sizeof(besselH02)/sizeof(besselH02[0]), bessel::H<0,2>);
+	bessel_tester(besselH02, sizeof(besselH02)/sizeof(besselH02[0]), NiHu::bessel::H<0,2>);
 }
 
 TEST(Bessel, H11) {
 	#include "data/besselH11.dat"
-	bessel_tester(besselH11, sizeof(besselH11)/sizeof(besselH11[0]), bessel::H<1,1>);
+	bessel_tester(besselH11, sizeof(besselH11)/sizeof(besselH11[0]), NiHu::bessel::H<1,1>);
 }
 
 TEST(Bessel, H12) {
 	#include "data/besselH12.dat"
-	bessel_tester(besselH12, sizeof(besselH12)/sizeof(besselH12[0]), bessel::H<1,2>);
+	bessel_tester(besselH12, sizeof(besselH12)/sizeof(besselH12[0]), NiHu::bessel::H<1,2>);
 }
 
 TEST(Bessel, K0) {
 	#include "data/besselK0.dat"
-	bessel_tester(besselK0, sizeof(besselK0)/sizeof(besselK0[0]), bessel::K<0>);
+	bessel_tester(besselK0, sizeof(besselK0)/sizeof(besselK0[0]), NiHu::bessel::K<0>);
 }
 
 TEST(Bessel, K1) {
 	#include "data/besselK1.dat"
-	bessel_tester(besselK1, sizeof(besselK1)/sizeof(besselK1[0]), bessel::K<1>);
+	bessel_tester(besselK1, sizeof(besselK1)/sizeof(besselK1[0]), NiHu::bessel::K<1>);
 }
 
---
+++ src/tutorial/laplace_bem_2d.mex.cpp
@@ -0,0 +1,18 @@
+#include "util/mex_matrix.hpp"
+#include "core/weighted_residual.hpp"
+#include "library/laplace_kernel.hpp"
+#include "library/laplace_singular_integrals.hpp"
+
+typedef NiHu::mex::real_matrix<double> dMatrix;
+
+void mexFunction(int nlhs, mxArray *lhs[], int nrhs, mxArray const *rhs[])
+{
+	dMatrix surf_nodes(rhs[0]), surf_elem(rhs[1]);
+	auto surf_mesh = NiHu::create_mesh(surf_nodes, surf_elem, NiHu::line_1_tag());
+	auto const &surf_sp = NiHu::constant_view(surf_mesh);
+	int n = surf_sp.get_num_dofs();
+	dMatrix L_surf(n, n, lhs[0]);
+	auto L = NiHu::create_integral_operator(NiHu::laplace_2d_SLP_kernel());
+	L_surf << dirac(surf_sp) * L[surf_sp];
+}
+
