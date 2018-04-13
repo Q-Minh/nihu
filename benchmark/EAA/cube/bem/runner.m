3ea0bc1ce4d09332f0626c1caceadd181d74a4d9
fiala <fiala@hit.bme.hu>
2018-04-13 15:53:26 +0200

    new files

--- benchmark/EAA/cube/bem/runner.m
+++ benchmark/EAA/cube/bem/runner.m
@@ -10,7 +10,7 @@ system('bem_const_bm.exe data/cube_quad.off data/points.off data/quad_const.xct 
 system('bem_gauss_bm.exe data/cube_quad.off data/points.off data/quad_gauss.xct data/quad_gauss_ps_bm.res data/quad_gauss_pf_bm.res');
 
 %% import
-freqs = .5 : .5 : 100;
+load data/freqs
 
 data = load('data/quad_const_ps_0.res', '-ascii');
 ps_c0 = complex(data(:,1:2:end), data(:,2:2:end)).';
@@ -27,52 +27,35 @@ ps_cb = complex(data(:,1:2:end), data(:,2:2:end)).';
 data = load('data/quad_const_pf_bm.res', '-ascii');
 pf_cb = complex(data(:,1:2:end), data(:,2:2:end)).';
 
+data = load('data/quad_gauss_ps_bm.res', '-ascii');
+ps_gb = complex(data(:,1:2:end), data(:,2:2:end)).';
+data = load('data/quad_gauss_pf_bm.res', '-ascii');
+pf_gb = complex(data(:,1:2:end), data(:,2:2:end)).';
+
 %%
 figure;
 formatfig();
 idx = 2;
-plot(freqs, 20*log10(abs(pf_cb(idx,:)/2e-5)), ...
-    freqs, 20*log10(abs(pf_g0(idx,:)/2e-5)), ...
-    freqs, 20*log10(abs(pf_c0(idx,:)/2e-5)));
+plot(freqvec, 20*log10(abs(pf_c0(idx,:)/2e-5)), ...
+    freqvec, 20*log10(abs(pf_cb(idx,:)/2e-5)), ...
+    freqvec, 20*log10(abs(pf_g0(idx,:)/2e-5)), ...
+    freqvec, 20*log10(abs(pf_gb(idx,:)/2e-5)));
 xlabel('Frequency [Hz]');
 ylabel('Sound pressure [dB]');
-legend('constant burton', 'gauss', 'constant', 'location', 'SouthEast');
+legend('constant', 'constan bm', 'gauss', 'gauss bm', 'location', 'SouthEast');
 setfig('FontSize', 12, 'LineWidth', 1);
 title(sprintf('Field Point Pressure in point %d', idx));
 
 %%
 figure;
 formatfig();
-idx = 24;
-plot(freqs_cb, 20*log10(abs(ps_cb(idx,:)/2e-5)), ...
-    freqs_gb, 20*log10(abs(ps_gb(idx,:)/2e-5)), ...
-    freqs_c0, 20*log10(abs(ps_c0(idx,:)/2e-5)), ...
-    freqs_g0, 20*log10(abs(ps_g0(idx,:)/2e-5)));
+idx = 100;
+plot(freqvec, 20*log10(abs(ps_c0(idx,:)/2e-5)), ...
+    freqvec, 20*log10(abs(ps_cb(idx,:)/2e-5)), ...
+    freqvec, 20*log10(abs(mean(ps_g0(4*idx-3:4*idx,:),1)/2e-5)), ...
+    freqvec, 20*log10(abs(mean(ps_gb(4*idx-3:4*idx,:),1)/2e-5)));
 xlabel('Frequency [Hz]');
 ylabel('Sound pressure [dB]');
-legend('constant burton', 'linear burton', 'constant', 'linear', 'location', 'SouthEast');
+legend('constant', 'constan bm', 'gauss', 'gauss bm', 'location', 'SouthEast');
 setfig('FontSize', 12, 'LineWidth', 1);
 title(sprintf('Surface Pressure in point %d', idx));
-
-%%
-figure;
-formatfig();
-idx = 1;
-plot(freqs_cb, iters_cb, ...
-    freqs_gb, iters_gb, ...
-    freqs_c0, iters_c0, ...
-    freqs_g0, iters_g0);
-xlabel('Frequency [Hz]');
-ylabel('#iterations [-]');
-legend('constant burton', 'linear burton', 'constant', 'linear', 'location', 'SouthEast');
-setfig('FontSize', 12, 'LineWidth', 1);
-
-%%
-mesh = import_off_mesh('data/cube_quad.off');
-points = import_off_mesh('data/points.off');
-figure;
-plot_mesh(mesh, real(ps_c0(:,42)));
-axis equal tight;
-shading flat
-hold on;
-plot3(points.Nodes(:,2), points.Nodes(:,3), points.Nodes(:,4), 'r*');
