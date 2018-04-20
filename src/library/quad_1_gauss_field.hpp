13aa9e19ae8c88758a3cf049e5c6f1a816b19282
Peter Fiala <fiala@hit.bme.hu>
2018-04-20 08:41:58 +0200

    quad_1_gauss_shape set and field added

---
+++ src/library/quad_1_gauss_field.hpp
@@ -0,0 +1,34 @@
+#ifndef QUAD_1_GAUSS_FIELD_HPP_INCLUDED
+#define QUAD_1_GAUSS_FIELD_HPP_INCLUDED
+
+#include "quad_1_gauss_shape_set.hpp"
+#include "core/field.hpp"
+#include "lib_element.hpp"
+
+// define traits of the new field type
+namespace NiHu
+{
+
+// define the new field type
+typedef field<
+	quad_1_elem,			// over a linear quad elem
+	quad_1_gauss_shape_set,	// using the new shape set
+	_1d 					// scalar field
+> quad_1_gauss_field;
+
+namespace field_traits
+{
+	template <>
+	struct id<quad_1_gauss_field> { enum {value = 666}; };
+} // end of namespace field_traits
+
+// define a tag to the new type
+struct quad_1_gauss_field_tag {};
+
+template <>
+struct tag2field<quad_1_gauss_field_tag> :
+	quad_1_gauss_field {};
+	
+} // end of namespace NiHu
+
+#endif // QUAD_1_GAUSS_FIELD_HPP_INCLUDED
