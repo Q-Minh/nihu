ee0114b66fdfa01e4ff8d9121900b8feafe3603c
Peter Fiala <fiala@hit.bme.hu>
2018-04-26 14:51:35 +0200

    mivans

--- src/tutorial/kl1d_test.m
+++
@@ -1,7 +0,0 @@
-clear;
-mesh = create_line(1, 100);
-sigma = 2;
-d = .5;
-[nodes, elements] = extract_core_mesh(mesh);
-elements(:,1) = 11202; 	% convert to volume lines
-[G, lambda] = kl1d(nodes, elements, sigma, d);
