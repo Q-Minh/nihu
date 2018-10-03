4e32888a646c5bcb84da8469eaa31ba144f8b0b8
fiala <fiala@hit.bme.hu>
2018-10-03 17:02:32 +0200

    be_invmap and cmake for MSVC

--- src/matlab/meshing/be_invmap.m
+++ src/matlab/meshing/be_invmap.m
@@ -1,4 +1,20 @@
-function [xi, niter] = be_invmap(coords, x0, shapeset)
+function [xi, err, niter] = be_invmap(coords, x0, shapeset, tol, maxiter)
+%be_invmap inverse mapping on surface elements
+%   [xi, err, niter] = be_invmap(coords, x0, shapeset, tol, maxiter)
+%   computes the intrinsic coordinate of point x0 over the element defined
+%   by coordinates coord and the geometrical shape set shapeset.
+%   xi(1:end-1) contains the intrinsic coordinates, and xi(end) contains
+%   the normal distance from the element. The algorithm uses a Newton
+%   Raphson iteration. The number of iterations is returned in niter.
+
+% default max number of iterations
+if nargin < 5
+    maxiter = 100;
+end
+% default tolerance
+if nargin < 4
+    tol = 1e-6;
+end
 
 coords = coords.';
 
@@ -7,67 +23,53 @@ dim = shapeset.Domain.Space.Dimension;
 xi0 = zeros(1, dim);
 zeta = 0;
 
-
-% rotates pi/2 in positive direction if applied from the left
-
 % Newton Raphson cycle
-eps = 1e-8;
-niter = 1;
-while true
+for niter = 1 : maxiter
     [N, dN, ddN] = shapeset.eval(xi0);
     x = N * coords;
     
     if (dim == 1)
         dx = dN * coords;
         ddx = ddN * coords;
-        
-        T = [0 -1; 1 0];
+        T = [0 -1; 1 0];    % rotates pi/2 in positive direction
         n = (T * dx.').';
-        
         dn = (T * ddx.').';
-        
     elseif (dim == 2)
         dxxi = dN(:,:,1) * coords;
         dxeta = dN(:,:,2) * coords;
         n = cross(dxxi, dxeta, 2);
-        
         dxxixi = ddN(:,:,1,1) * coords;
         dxxieta = ddN(:,:,1,2) * coords;
         dxetaeta = ddN(:,:,2,2) * coords;
-        
         dnxi = cross(dxxixi, dxeta, 2) + cross(dxxi, dxxieta, 2);
         dneta = cross(dxxieta, dxeta, 2) + cross(dxxi, dxetaeta, 2);
     end
     
     f = (x + n*zeta) - x0;
-    if norm(f) < eps
-        break;
+    err = norm(f);
+    if err < tol
+        break
     end
     
-    if (dim == 1)
-    df = [
-        dx + dn*zeta
-        n
-        ];
-    elseif (dim == 2)
-    df = [
-        dxxi + dnxi*zeta
-        dxeta + dneta*zeta
-        n
-        ];
+    if dim == 1
+        df = [dx + dn*zeta; n];
+    elseif dim == 2
+        df = [
+            dxxi + dnxi*zeta
+            dxeta + dneta*zeta
+            n
+            ];
     end
     xi = [xi0 zeta] - (df.' \ f.').';
     
     xi0 = xi(1:end-1);
     zeta = xi(end);
-
-    niter = niter + 1;
+    
     if (niter > 100)
-        break;
+        break
     end
-
-end
+end % of NR iterations
 
 xi = [xi0 zeta];
 
-end
+end % of function be_invmap
