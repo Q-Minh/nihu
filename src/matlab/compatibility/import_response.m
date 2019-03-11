d3cf443a2cf33f1d74894c7a7cac9932aa0e2e52
fiala <fiala@hit.bme.hu>
2019-03-11 10:48:58 +0100

    import_response added

---
+++ src/matlab/compatibility/import_response.m
@@ -0,0 +1,16 @@
+function [p, k] = import_response(fname)
+%EXPORT_EXCITATION export a NiHu acoustic BEM excitation into a text file
+%   EXPORT_EXCITATION(Q, K, FNAME) exports the NiHu excitation Q at wave
+%   number K to the text file FNAME.
+
+fid = fopen(fname, 'rt');
+if fid == -1
+    error('NiHu:file_open', 'Cannot open file %s', fname);
+end
+
+k = fscanf(fid, '%g\n', 1);
+s = fscanf(fid, '%u\n', 1);
+data = fscanf(fid, '%g %g\n', [2, inf]);
+p = complex(data(1,:), data(2,:)).';
+fclose(fid);
+end
