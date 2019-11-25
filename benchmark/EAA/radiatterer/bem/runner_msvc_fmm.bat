1faa2c10bc9bad7d814da188329826a77bb69a9d
fiala <fiala@hit.bme.hu>
2019-11-25 18:13:01 +0100

    files

---
+++ benchmark/EAA/radiatterer/bem/runner_msvc_fmm.bat
@@ -0,0 +1,19 @@
+@ECHO OFF
+SET FREQ=300
+SET FREQSTR=%FREQ%Hz
+SET LE=100mm
+SET TYPE=const
+SET OUTDIR=data_fmm\%TYPE%_%LE%
+SET EXE=helmholtz_3d_hffmm_%TYPE%.exe
+ECHO ON
+
+%EXE% --solve --frequency %FREQ%^
+	--surface_mesh mesh\radiatterer_%LE%_quad.off^
+	--surface_result %OUTDIR%\%TYPE%_%LE%_%FREQSTR%_ps.res
+     
+%EXE% --postprocess --frequency %FREQ%^
+	--surface_mesh mesh\radiatterer_%LE%_quad.off^
+	--field_mesh mesh\radiatterer_points_quad.off^
+	--surface_result %OUTDIR%\%TYPE%_%LE%_%FREQSTR%_ps.res^
+	--field_result %OUTDIR%\%TYPE%_%LE%_%FREQSTR%_pf.res
+	
